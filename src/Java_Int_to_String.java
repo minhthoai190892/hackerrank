a8098b491275fdfa58b8d371d0c55d40871e9a0e
minhthoai <minhthoai190892@gmail.com>
2023-04-23 17:20:02 +0700

    Java_Int_to_String

---
+++ src/Java_Int_to_String.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Java_Int_to_String {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = scanner.nextInt();
+        String s = "";
+        // check n -100<n<100
+        if (n >= -100 && n <= 100) {
+            s = Integer.toString(n);
+
+        }
+        if (s.length() > 0) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");
+        }
+
+    }
+}
