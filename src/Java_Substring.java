01a29fe49baaf9341f1b5433f3fcb6b3109dcadb
minhthoai <minhthoai190892@gmail.com>
2023-04-27 20:17:35 +0700

    Java-Substring

---
+++ src/Java_Substring.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Java_Substring {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String s = scanner.nextLine();
+        System.out.println("Enter start: ");
+        int start = scanner.nextInt();
+        System.out.println("Enter end: ");
+        int end = scanner.nextInt();
+        System.out.println("start: " + start + " " + "end " + end);
+        System.out.println(s.substring(start, end));
+    }
+}
